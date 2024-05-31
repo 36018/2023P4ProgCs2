f1aae30592dd08979adbe6548ae4b51f18d49b98
36018 <36018@ma-web.nl>
2024-05-31 13:08:23 +0200

    09 files and directories

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+using System;
+using System.IO;
+
+namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Celin\Documents\mediacollege\bewijzenmap\periode 4\prog\2023P4ProgCs2");
+
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] directories = dir.GetDirectories();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+            for (int i = 0; i < directories.Length; i++)
+            {
+                Console.WriteLine(directories[i]);
+            }
+
+        }
+    }
+}

eb4f4234c78401fcb94542e0bfce80180fd1ad97
36018 <36018@ma-web.nl>
2024-06-03 12:48:56 +0200

    10 fileio

---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("leesdezefile.txt");
+            
+            for (int i = 0; i < content.Length; i++)
+            {
+                Console.WriteLine(content[i]);
+            }
+
+            File.Create("mijnnieuwefile.txt").Dispose();
+            File.AppendText("Append");
+
+            Directory.CreateDirectory("output");
+
+            File.Move("mijnnieuwefile.txt","output/mijnnieuwefile.txt");
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

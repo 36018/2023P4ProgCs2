471cd5cabd68b439fe9d73f91df7fd7e68152b82
36018 <36018@ma-web.nl>
2024-06-21 15:01:58 +0200

    12a

--- ConsoleMonGame/ConsoleMonGame/ConsoleMonFactory.cs
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMonFactory.cs
@@ -10,12 +10,21 @@ namespace ConsoleMonGame
     {
         internal void Load(string datafile)
         {
-           string[] lines = File.ReadAllLines(datafile);
+            string[] lines = File.ReadAllLines(datafile);
 
             for (int i = 0; i < lines.Length; i++)
             { 
-                Console.WriteLine(lines[i]);
-            }
+                string[] typeSplit = lines[i].Split("|");
+                string[] consoleMonData = typeSplit[0].Split(",");
+                Console.WriteLine(consoleMonData[i]);
+
+                ConsoleMon dataMon = new ConsoleMon();
+                dataMon.name = consoleMonData[0];
+                dataMon.energy = int.Parse(consoleMonData[4]);
+                dataMon.health = int.Parse(consoleMonData[2]);
+
+                Console.WriteLine(dataMon.name + " " + dataMon.energy + " " + dataMon.health);
+            }  
         }
     }
 }
